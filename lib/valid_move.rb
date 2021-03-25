34939d2c6320b1ad891177f656e34392d6318ef5
Christopher Black <christopher.black.2209@googlemail.com>
2021-03-25 21:35:53 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,6 @@
-def valid_move?(board, index)
-  if index.between?(0,8) && position_taken?(board, index) == false
-    return true
-  else
-    return false 
-  end
-end
+def position_taken?(board, index)
+!(board[index] == " " || board[index] == "" || board[index] == nil)
+end 
 
 def position_taken?(board, index)
     !(board[index] == " " || board[index] == "" || board[index] == nil)
